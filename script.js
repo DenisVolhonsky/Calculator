914584f97368baa583b5e3a8ebd90af17d9eecd6
DenisVolhonsky <dvolhonsky@gmail.com>
2017-07-04 13:23:29 +0300

    only web layout

---
+++ script.js
@@ -0,0 +1,25 @@
+function plus() {
+	result.value='+';
+}
+plus();
+function minus() {
+	result.value='-';
+}
+minus();
+function divine() {
+	result.value='/';
+}
+divine();
+function mult() {
+	result.value='*';
+}
+mult();
+function equal() {
+	result.value='=';
+}
+
+function w() {
+
+	result.value=document.getElementById('seven').value;
+}
+w();
